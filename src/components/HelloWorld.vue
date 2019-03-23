a787d2d81c28943797b8ef768af1817c4f2c390e
tomsure <491583156@qq.com>
2019-03-23 19:49:04 +0800

    用vue构建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>网站更新中...敬请期待</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style  scoped>
+</style>
